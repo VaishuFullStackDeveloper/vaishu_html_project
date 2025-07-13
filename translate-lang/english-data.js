d2cfcd0f470a78e16d0dfcec631d27834b764a59
VaishuFullStackDeveloper <vaishalidewase9@gmail.com>
2025-07-12 23:36:50 -0700

    Add files via upload

---
+++ translate-lang/english-data.js
@@ -0,0 +1,12 @@
+const englishData = {
+heading:"Cloud AI",
+paragraph:"Cloud AI refers to the integration of Artificial Intelligence (AI) and cloud computing, enabling organizations to leverage AI capabilities through cloud platforms. This fusion allows businesses to access and utilize AI tools, models, and infrastructure without the need for significant upfront investment in hardware or specialized expertise. Cloud AI offers benefits like enhanced automation, improved decision-making, and increased scalability for various business processes",
+admission_process_heading:"Admission process steps",
+admission_process_list:`<li>Reasearch collage and courses</li>
+<li>fill out application form</li>
+<li>Appears for enterance </li>
+<li>attend counselling sessions</li>
+<li>submit required documents and fee</li>`
+
+
+};
---
+++ translate-lang/spanish-data.js
@@ -0,0 +1,11 @@
+const spanishData = {
+heading:"IA en la nube",
+paragraph:"La IA en la nube se refiere a la integración de la Inteligencia Artificial (IA) y la computación en la nube, lo que permite a las organizaciones aprovechar las capacidades de IA a través de plataformas en la nube. Esta fusión permite a las empresas acceder y utilizar herramientas, modelos e infraestructura de IA sin necesidad de una inversión inicial significativa en hardware ni experiencia especializada. La IA en la nube ofrece beneficios como una mayor automatización, una mejor toma de decisiones y una mayor escalabilidad para diversos procesos empresariales.",
+admission_process_heading:"Pasos del proceso de admisión",
+admission_process_list: `<li>Investigación universitaria y cursos</li>
+<li>Llenar el formulario de solicitud</li>
+<li>Presentarse para el ingreso</li>
+<li>Asistir a las sesiones de orientación</li>
+<li>Entregar la documentación requerida y pagar la cuota</li>`
+
+};
